6fb84796fb4d067a9b5bd24f627bda3d73f54aa2
bushera <59298086+bushera@users.noreply.github.com>
2020-10-06 16:59:30 +0100

    update

--- topbar.js
+++ topbar.js
@@ -11,27 +11,6 @@ $(function(){
 		});
   
 		
-		$("#year-2018").click(function(){
-			$("#slide2").css("display", "none");
-			$("#slide3").css("display", "none");
-			$("#slide1").css("display", "block");
-			$("#year-2018").off(click);
-		});
-		
-		$("#year-2019").click(function(){
-			$("#slide1").animate({display: "none"}, 500);
-			$("#slide3").css("display", "none");
-			$("#slide2").css("display", "block");
-			$("#year-2019").off(click);
-		});
-		
-		$("#year-2020").click(function(){
-			$("#slide1").animate({display: "none"}, 500);
-			$("#slide2").css("display", "none");
-			$("#slide3").css("display", "block");
-			$("#year-2020").off(click);
-		});
-		
 		
 		$("#year-2018a").click(function(){
 			$("#slide2a").css("display", "none");
@@ -89,6 +68,65 @@ $(function(){
 			$("#year-collapse").css("display", "block");
 			$("#year-collapseb").css("display", "none");
 			$("#year-collapseb").off(click);
+		});
+		
+		
+		$("#year-2018ai").click(function(){
+			$("#slide2").css("display", "none");
+			$("#slide3").css("display", "none");
+			$("#slide1").css("display", "block");
+			$("#year1i").css("display", "block");
+			$("#year2i").css("display", "none");
+			$("#year3i").css("display", "none");
+			$("#year-dropi").animate({height: "0%"}, 500);
+			$("#year-dropi").css("display", "none");
+			$("#year-collapsei").css("display", "block");
+			$("#year-collapsebi").css("display", "none");
+			$("#year-2018ai").off(click);
+		});
+		
+		$("#year-2019ai").click(function(){
+			$("#slide1").animate({display: "none"}, 500);
+			$("#slide3").css("display", "none");
+			$("#slide2").css("display", "block");
+			$("#year1i").css("display", "none");
+			$("#year2i").css("display", "block");
+			$("#year3i").css("display", "none");
+			$("#year-dropi").animate({height: "0%"}, 500);
+			$("#year-dropi").css("display", "none");
+			$("#year-collapsei").css("display", "block");
+			$("#year-collapsebi").css("display", "none");
+			$("#year-2019ai").off(click);
+		});
+		
+		$("#year-2020ai").click(function(){
+			$("#slide1").animate({display: "none"}, 500);
+			$("#slide2").css("display", "none");
+			$("#slide3").css("display", "block");
+			$("#year1i").css("display", "none");
+			$("#year2i").css("display", "none");
+			$("#year3i").css("display", "block");
+			$("#year-dropi").animate({height: "0%"}, 500);
+			$("#year-dropi").css("display", "none");
+			$("#year-collapsei").css("display", "block");
+			$("#year-collapsebi").css("display", "none");
+			$("#year-2020ai").off(click);
+		});
+		
+		$("#year-collapsei").click(function(){
+			$("#year-dropi").css("display", "block");
+			$("#year-dropi").animate({height: "15%"}, 500);
+			$("#year-collapsei").css("display", "none");
+			$("#year-collapsebi").css("display", "block");
+			$("#year-collapsei").off(click);
+		});
+		
+		$("#year-collapsebi").click(function(){
+			$("#year-dropi").animate({height: "0%"}, 500);
+			$("#year-dropi").css("display", "none");
+			$("#year-collapsei").css("display", "block");
+			$("#year-collapsebi").css("display", "none");
+			$("#year-collapsebi").off(click);
 		});
 		
 		
